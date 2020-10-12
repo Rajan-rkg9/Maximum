eb1ca99e33e35d5f8bb2e2b5fe4e43909e48753b
Rajan-rkg9 <amirajangupta@gmail.com>
2020-10-12 20:03:10 +0530

    [Rajan-rkg9] Find. Maximum of Three Floating numbers

--- FindMaximumTest.java
+++ FindMaximumTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class FindMaximumTest {
 
 	@Test
-	public void givenMaxIntegerNumber_At1stPosition_ShouldReturnTheSameNumber() {
+	public void givenMaxFloatingNumber_At1stPosition_ShouldReturnTheSameNumber() {
 		FindMaximum testMax=new FindMaximum();
 		Float a=79.0f, b=34.0f, c=65.7f;
 		Float maxFloat=testMax.findMaxFloat(a,b,c);
@@ -15,7 +15,7 @@ public class FindMaximumTest {
 	}
 	
 	@Test
-	public void givenMaxIntegerNumber_At2ndPosition_ShouldReturnTheSameNumber() {
+	public void givenMaxFloatingNumber_At2ndPosition_ShouldReturnTheSameNumber() {
 		FindMaximum testMax=new FindMaximum();
 		Float a=23.7f, b=120.7f, c=48.4f;
 		Float maxFloat=testMax.findMaxFloat(a,b,c);
@@ -23,7 +23,7 @@ public class FindMaximumTest {
 	}
 	
 	@Test
-	public void givenMaxIntegerNumber_At3rdPosition_ShouldReturnTheSameNumber() {
+	public void givenMaxFloatingNumber_At3rdPosition_ShouldReturnTheSameNumber() {
 		FindMaximum testMax=new FindMaximum();
 		Float a=12.8f, b=39.7f, c=120.4f;
 		Float maxFloat=testMax.findMaxFloat(a,b,c);
